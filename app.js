0c6e0950e6c73793628e2b0cb5df454be8d0212f
Timofey Rechkalov <trechkalov@yandex.ru>
2016-09-07 04:22:10 +0500

    Added stream error stub. Mail address separated to fromMail and toMail addresses.

--- app.js
+++ app.js
@@ -6,27 +6,34 @@ var fstream = require('fstream');
 var tar = require('tar');
 var config = require('./config');
 
-var transporter = nodemailer.createTransport(['smtps://', config.get('mail'), ':', config.get('password'), '@' , 
+var transporter = nodemailer.createTransport(['smtps://', config.get('fromMail'), ':', config.get('password'), '@' , 
                                               config.get('smtpServer')].join(''));
 
 var date = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: config.get('mail'), // sender address
-    to: config.get('mail'), // list of receivers
-    subject: config.get('backupName') + ' - ' + date, // Subject line
+    from: config.get('fromMail'), // sender address
+    to: config.get('toMail'), // list of receivers
     text: 'Test', // plaintext body,
 };
 
 // send mail with defined transport object
 setInterval(function(){
+//setTimeout(function(){
+    var date = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
+    var stream = fstream.Reader({ 'path': config.get('folderPath'), 'type': 'Directory' });
+    stream.on('error', function(e){
+      console.log(e.name + ': ' + e.message);
+      stream.resume();
+    });
     mailOptions.attachments = [{
         filename: 'archive_' + date + '.tar.gz',
-        content: fstream.Reader({ 'path': config.get('folderPath'), 'type': 'Directory' })
+        content: stream
         .pipe(tar.Pack())
         .pipe(zlib.Gzip())
     }];
+    mailOptions.subject = config.get('backupName') + ' - ' + date, // Subject line
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
             return console.log(error);
@@ -34,3 +41,4 @@ setInterval(function(){
         console.log('Message sent: ' + info.response);
     });
 }, config.get('timeInterval'));
+//}, 30000);
