01473e293fccde5f720f118863e1f210ea927bf0
NtsDK <ghoul71@yandex.ru>
2017-02-26 19:30:22 +0500

    Improved email backup - added filter of copied files, added logging.

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ var zlib = require('zlib');
 var fstream = require('fstream');
 var tar = require('tar');
 var config = require('./config');
+var logger = require('winston');
+var path = require('path');
+
+var dateFun = () => dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
+
+var logInfo = str => logger.info('[' + dateFun() + '] ' + str);
+var logError = str => logger.error('[' + dateFun() + '] ' + str);
 
 var transporter = nodemailer.createTransport(['smtps://', config.get('fromMail'), ':', config.get('password'), '@' , 
                                               config.get('smtpServer')].join(''));
 
-var date = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
+
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
@@ -19,26 +26,39 @@ var mailOptions = {
 };
 
 // send mail with defined transport object
-setInterval(function(){
-//setTimeout(function(){
-    var date = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
-    var stream = fstream.Reader({ 'path': config.get('folderPath'), 'type': 'Directory' });
-    stream.on('error', function(e){
-      console.log(e.name + ': ' + e.message);
-      stream.resume();
-    });
+function makeBackup(){
+	logInfo('Backup started');
+	var date = dateFun();
+	var stream = fstream.Reader({ 'path': config.get('folderPath'), 'type': 'Directory' , filter: function () {
+		if(this.basename === 'nims-base2.json' || this.basename === 'nims-base3.json' || this.basename === 'nims.log'){
+			return false;
+		}
+		return true;
+	}});
+	stream.on('error', function(e){
+	  logError(e.name + ': ' + e.message);
+	  stream.resume();
+	});
     mailOptions.attachments = [{
         filename: 'archive_' + date + '.tar.gz',
         content: stream
         .pipe(tar.Pack())
         .pipe(zlib.Gzip())
     }];
-    mailOptions.subject = config.get('backupName') + ' - ' + date, // Subject line
+	mailOptions.subject = config.get('backupName') + ' - ' + date, // Subject line
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
-            return console.log(error);
+            return logError(error);
         }
-        console.log('Message sent: ' + info.response);
+        logInfo('Message sent: ' + info.response);
+		logInfo('Backup finished successfully');
     });
-}, config.get('timeInterval'));
-//}, 30000);
+};
+
+if(config.get('singleRun')){
+	logInfo('Single run enabled');
+} else {
+	logInfo('Interval run enabled');
+	setInterval(makeBackup, config.get('timeInterval'));	
+}
+makeBackup();
